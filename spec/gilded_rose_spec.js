9bb4d2cf3edf9db2f7b161f7b16908ba69a3f2d6
Wagner Jesus <wagner.jesus@hotmart.com>
2022-02-15 17:35:33 -0300

    test: add gilded rose test structure

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,23 @@
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
+  it("should degrade quality twice if sell_in is less than zero", function() {
     update_quality();
   });
+
+  //should throw if quality is negative
+
+  //should increase quality when Aged Brie gets older
+
+  //should throw if quality is greater than 50
+
+  //shoulf throw if tries to sell Sulfuras
+
+  //should increase Backstage pass quality by 2 when 10 days or less
+
+  //should increase Backstage pass quality by 3 when 5 days or less
+
+  //should drop Backstage pass quality to 0 after the sell_in date
+
+  //should Conjured itens degrees quality twice as normal
 
 });
