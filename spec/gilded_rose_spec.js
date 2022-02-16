b7d7cc38a6ffb351443d436a36fa706cdc2a6c2e
Wagner Jesus <wagner.jesus@hotmart.com>
2022-02-16 19:25:45 -0300

    test: move item update to sut

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,103 +1,97 @@
 const { Item } = require('../src/item')
 const { update_quality } = require('../src/gilded_rose')
+const { ITEM_TYPES } = require("../src/constants");
+
+const makeSut = (sell_in, quality, name = 'test') => {
+  const newItem = [new Item(name, sell_in, quality)]
+  const updateQualityItems = update_quality(newItem)
+
+  return updateQualityItems.shift()
+}
 
 describe("Gilded Rose", function() {
 
-  it("should decrease quality every day", function() {
-    const newItem = [new Item('test', 4, 6)]
-    const updateQualityItems = update_quality(newItem)
+  it("should decrease quality every day", () => {
+    const { sell_in, quality} = makeSut(4, 6)
 
-    expect(updateQualityItems[0].sell_in).toEqual(3)
-    expect(updateQualityItems[0].quality).toEqual(5)
+    expect(sell_in).toEqual(3)
+    expect(quality).toEqual(5)
   });
 
   it("should degrade quality twice if sell_in is less than zero", () => {
-    const newItem = [new Item('test', -1, 6)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(-1, 6)
 
-    expect(updateQualityItems[0].quality).toEqual(4)
-    expect(updateQualityItems[0].sell_in).toEqual(-2)
+    expect(sell_in).toEqual(-2)
+    expect(quality).toEqual(4)
   })
 
   it("should not degrade quality if is zero", () => {
-    const newItem = [new Item('test', 3, 0)]
-    const updateQualityItems = update_quality(newItem)
+    const { quality} = makeSut(3, 0)
 
-    expect(updateQualityItems[0].quality).toEqual(0)
+    expect(quality).toEqual(0)
   })
 
   it("should increase quality if is Aged Brie when gets older", () => {
-    const newItem = [new Item('Aged Brie', 3, 3)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(3, 3, ITEM_TYPES.BRIE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(2)
-    expect(updateQualityItems[0].quality).toEqual(4)
+    expect(sell_in).toEqual(2)
+    expect(quality).toEqual(4)
   })
 
   it("should not increase quality if is fifty", () => {
-    const newItem = [new Item('Aged Brie', 3, 50)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(3, 50, ITEM_TYPES.BRIE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(2)
-    expect(updateQualityItems[0].quality).toEqual(50)
+    expect(sell_in).toEqual(2)
+    expect(quality).toEqual(50)
   })
 
   it("should not sell Sulfuras", () => {
-    const newItem = [new Item('Sulfuras', 3, 3)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(3, 3, ITEM_TYPES.SULFURAS)
 
-    expect(updateQualityItems[0].sell_in).toEqual(3)
-    expect(updateQualityItems[0].quality).toEqual(3)
+    expect(sell_in).toEqual(3)
+    expect(quality).toEqual(3)
   })
 
   it("should increase Backstage pass quality by 2 when 10 days", () => {
-    const newItem = [new Item('Backstage', 10, 3)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(10, 3, ITEM_TYPES.BACKSTAGE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(9)
-    expect(updateQualityItems[0].quality).toEqual(5)
+    expect(sell_in).toEqual(9)
+    expect(quality).toEqual(5)
   })
 
   it("should increase Backstage pass quality by 2 when less 10 days", () => {
-    const newItem = [new Item('Backstage', 9, 3)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(9, 3, ITEM_TYPES.BACKSTAGE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(8)
-    expect(updateQualityItems[0].quality).toEqual(5)
+    expect(sell_in).toEqual(8)
+    expect(quality).toEqual(5)
   })
 
   it("should increase Backstage pass quality by 3 when 5 days", () => {
-    const newItem = [new Item('Backstage', 5, 3)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(5, 3, ITEM_TYPES.BACKSTAGE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(4)
-    expect(updateQualityItems[0].quality).toEqual(6)
+    expect(sell_in).toEqual(4)
+    expect(quality).toEqual(6)
   })
 
   it("should increase Backstage pass quality by 3 when lesse than 5 days", () => {
-    const newItem = [new Item('Backstage', 4, 3)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(4, 3, ITEM_TYPES.BACKSTAGE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(3)
-    expect(updateQualityItems[0].quality).toEqual(6)
+    expect(sell_in).toEqual(3)
+    expect(quality).toEqual(6)
   })
 
   it("should drop Backstage pass quality to 0 if sell_in is less than 0", () => {
-    const newItem = [new Item('Backstage', 0, 5)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(0, 5, ITEM_TYPES.BACKSTAGE)
 
-    expect(updateQualityItems[0].sell_in).toEqual(-1)
-    expect(updateQualityItems[0].quality).toEqual(0)
+    expect(sell_in).toEqual(-1)
+    expect(quality).toEqual(0)
   })
 
   it("should decrease Conjured quality twice", () => {
-    const newItem = [new Item('Conjured', 5, 5)]
-    const updateQualityItems = update_quality(newItem)
+    const { sell_in, quality} = makeSut(5, 5, ITEM_TYPES.CONJURED)
 
-    expect(updateQualityItems[0].sell_in).toEqual(4)
-    expect(updateQualityItems[0].quality).toEqual(3)
+    expect(sell_in).toEqual(4)
+    expect(quality).toEqual(3)
   })
-
-  //should Conjured itens degrees quality twice as normal
 
 });
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,13 @@
-const QUALITY_TAX = 1
+const QUALITY_TAX = 1;
 
+const ITEM_TYPES = {
+  BRIE: "Aged Brie",
+  BACKSTAGE: "Backstage",
+  SULFURAS: "Sulfuras",
+  CONJURED: "Conjured",
+};
 
 module.exports = {
-    QUALITY_TAX
-}
+  QUALITY_TAX,
+  ITEM_TYPES,
+};
