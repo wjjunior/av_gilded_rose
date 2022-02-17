5da307215b9bc35b01b0a7060adfc36fe5b69a99
Wagner Jesus <wagner.jesus@hotmart.com>
2022-02-17 11:07:15 -0300

    refactor: improve gilded rose code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,73 +1,46 @@
-const { Item } = require('./item')
-const { QUALITY_TAX } = require('./constants')
+const { Item, updateItems } = require("./item");
+const { ITEM_TYPES } = require("./constants");
 
-var items = []
+var items = [];
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
+items.push(new Item("+5 Dexterity Vest", 10, 20));
+items.push(new Item("Aged Brie", 2, 0));
+items.push(new Item("Elixir of the Mongoose", 5, 7));
+items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+items.push(new Item("Conjured Mana Cake", 3, 6));
 
-function update_quality(items) {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && !(items[i].name).includes('Backstage')) {
-      if (items[i].quality > 0) {
-        if (!(items[i].name).includes('Sulfuras')) {
-          items[i].quality = items[i].quality - QUALITY_TAX
-        }
-        if((items[i].name).includes('Conjured')) {
-          items[i].quality = items[i].quality - QUALITY_TAX
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + QUALITY_TAX
-        if ((items[i].name).includes('Backstage')) {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + QUALITY_TAX
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + QUALITY_TAX
-            }
-          }
-          if(items[i].sell_in < 1) {
-            items[i].quality = 0
-          }
-        }
-      }
+function update_quality(items = items) {
+  return items.map((item) => {
+    let newItem;
+    const updateItemsfn = updateItems(item.sell_in, item.quality);
+
+    if (item.name.includes(ITEM_TYPES.BRIE)) {
+      newItem = updateItemsfn.agedBrie();
     }
-    if (!(items[i].name).includes('Sulfuras')) {
-      items[i].sell_in = items[i].sell_in - 1;
-      if((items[i].name).includes('Backstage') && items[i].sell_in < 0) {
-        items[i].quality = 0
-      }
+
+    if (item.name.includes(ITEM_TYPES.BACKSTAGE)) {
+      newItem = updateItemsfn.backstage();
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (!(items[i].name).includes('Backstage')) {
-          if (items[i].quality > 0) {
-            if (!(items[i].name).includes('Sulfuras')) {
-              items[i].quality = items[i].quality - QUALITY_TAX
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + QUALITY_TAX
-        }
-      }
+
+    if (item.name.includes(ITEM_TYPES.SULFURAS)) {
+      newItem = updateItemsfn.sulfuras();
     }
-  }
-  return items
+
+    if (item.name.includes(ITEM_TYPES.CONJURED)) {
+      newItem = updateItemsfn.conjured();
+    }
+
+    if (!newItem) {
+      newItem = updateItemsfn.random();
+    }
+
+    item.sell_in = newItem.sellIn;
+    item.quality = newItem.quality;
+    return item;
+  });
 }
 
 module.exports = {
-  update_quality
-}
+  update_quality,
+};
--- src/item.js
+++ src/item.js
@@ -1,10 +1,68 @@
+const { QUALITY_TAX } = require("./constants");
+
 function Item(name, sell_in, quality) {
-    this.name = name;
-    this.sell_in = sell_in;
-    this.quality = quality;
+  this.name = name;
+  this.sell_in = sell_in;
+  this.quality = quality;
 }
 
+function backstageQuality(sellIn, quality) {
+  if (sellIn < 0) return 0;
+  if (sellIn <= 5) return quality + 3;
+  if (sellIn <= 10) return quality + 2;
+  return quality + QUALITY_TAX;
+}
+
+function updateItems(sellIn, quality) {
+  function agedBrie() {
+    --sellIn;
+    return {
+      sellIn,
+      quality: Math.min(50, quality + QUALITY_TAX),
+    };
+  }
+  function backstage() {
+    --sellIn;
+    quality = backstageQuality(sellIn, quality);
+
+    return {
+      sellIn,
+      quality: Math.min(50, quality),
+    };
+  }
+  function sulfuras() {
+    return {
+      sellIn,
+      quality,
+    };
+  }
+  function conjured() {
+    return {
+      sellIn: --sellIn,
+      quality: Math.max(0, quality - QUALITY_TAX * 2),
+    };
+  }
+  function random() {
+    --sellIn;
+    return {
+      sellIn,
+      quality: Math.max(
+        sellIn > 0 ? quality - QUALITY_TAX : quality - QUALITY_TAX * 2,
+        0
+      ),
+    };
+  }
+
+  return {
+    agedBrie,
+    backstage,
+    sulfuras,
+    conjured,
+    random,
+  };
+}
 
 module.exports = {
-    Item
-}
+  Item,
+  updateItems,
+};
